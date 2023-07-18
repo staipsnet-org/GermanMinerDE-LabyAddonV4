7f04df256f35358ad7248e67e1745a6110f7f800
Funboyy <72627658+Funboyy@users.noreply.github.com>
2023-07-18 23:47:41 +0200

    added needle for speed, speed limiter and fuel

--- core/src/main/java/de/germanminer/addon/widgets/VehicleDisplayWidget.java
+++ core/src/main/java/de/germanminer/addon/widgets/VehicleDisplayWidget.java
@@ -2,19 +2,36 @@ package de.germanminer.addon.widgets;
 
 import de.germanminer.addon.GermanMinerAddon;
 import de.germanminer.addon.api.protocol.packet.vehicle.VehicleDisplayPacket;
+import net.labymod.api.Laby;
 import net.labymod.api.client.component.Component;
 import net.labymod.api.client.component.TranslatableComponent;
 import net.labymod.api.client.gui.hud.hudwidget.HudWidgetConfig;
 import net.labymod.api.client.gui.hud.hudwidget.widget.WidgetHudWidget;
+import net.labymod.api.client.gui.hud.position.HudSize;
 import net.labymod.api.client.gui.icon.Icon;
+import net.labymod.api.client.gui.mouse.MutableMouse;
 import net.labymod.api.client.gui.screen.widget.widgets.hud.HudWidgetWidget;
+import net.labymod.api.client.render.matrix.Stack;
 import net.labymod.serverapi.protocol.packet.PacketHandler;
 
 public class VehicleDisplayWidget extends WidgetHudWidget<HudWidgetConfig> implements PacketHandler<VehicleDisplayPacket> {
 
+  private static final float SPEED_NEEDLE_MAX = -225F;
+  private static final float FUEL_NEEDLE_MAX = 90F;
+  private static final float SPEED_NEEDLE_UNIT = SPEED_NEEDLE_MAX / 200F;
+  private static final float FUEL_NEEDLE_UNIT = FUEL_NEEDLE_MAX / 100F;
+
   private final Icon hudWidgetIcon;
   private VehicleDisplayContent content;
+
   private boolean show;
+  private boolean nightMode;
+  private int speed;
+  private int fuel;
+  private boolean limiterActive;
+  private int limiterSpeed;
+  private int damageState;
+  private int warningLight;
 
   public VehicleDisplayWidget(final String id) {
     super(id, HudWidgetConfig.class);
@@ -38,10 +55,49 @@ public class VehicleDisplayWidget extends WidgetHudWidget<HudWidgetConfig> imple
   }
 
   @Override
+  public void render(final Stack stack, final MutableMouse mouse, final float partialTicks,
+      final boolean isEditorContext, final HudSize size) {
+
+    drawLine(stack, size, Math.max(0, Math.min(SPEED_NEEDLE_MAX, this.speed * SPEED_NEEDLE_UNIT)),
+        VehicleDisplayTexture.SPEED_NEEDLE.getIcon(this.nightMode));
+    drawLine(stack, size, Math.max(0, Math.min(FUEL_NEEDLE_MAX, this.fuel * FUEL_NEEDLE_UNIT)),
+        VehicleDisplayTexture.FUEL_NEEDLE.getIcon(this.nightMode));
+
+    if (this.limiterSpeed >= 10) {
+      final Icon icon = this.limiterActive ?
+          VehicleDisplayTexture.LIMITER_NEEDLE.getIcon(this.nightMode) :
+          VehicleDisplayTexture.LIMITER_NEEDLE_INACTIVE.getIcon(this.nightMode);
+
+      drawLine(stack, size, Math.max(0, Math.min(SPEED_NEEDLE_MAX, this.limiterSpeed * SPEED_NEEDLE_UNIT)), icon);
+    }
+
+    if (this.damageState >= 1) {
+      final boolean visible = this.warningLight++ < 40;
+
+      if (this.content.getWarning().isVisible() != visible) {
+        this.content.getWarning().setVisible(visible);
+      }
+
+      if (this.warningLight == 80) {
+        this.warningLight = 0;
+      }
+    }
+  }
+
+  private void drawLine(final Stack stack, final HudSize size, final float angle, final Icon icon) {
+    stack.push();
+    stack.translate(size.getWidth() / 2F, size.getHeight() / 2F, 3);
+    stack.rotate(angle, 0F, 0F, 1F);
+    stack.translate(-size.getWidth() / 2F, -size.getHeight() / 2F, 3);
+    icon.render(stack, 0F, 0F, size.getWidth(), size.getHeight());
+    stack.translate(0F, 0F, 0F);
+    stack.pop();
+  }
+
+  @Override
   public boolean isVisibleInGame() {
-    return true;
-    //return GermanMinerAddon.getInstance().enabled()
-    // && !Laby.references().chatAccessor().isChatOpen() && this.show;
+    return GermanMinerAddon.getInstance().enabled()
+     && !Laby.references().chatAccessor().isChatOpen() && this.show;
   }
 
   @Override
@@ -55,43 +111,36 @@ public class VehicleDisplayWidget extends WidgetHudWidget<HudWidgetConfig> imple
       }
     }
 
-    drawSpeedometer(packet);
+    // update night mode
+    if (packet.getNightMode() != null) {
+      this.content.getBackground().icon().set(VehicleDisplayTexture.SPEEDOMETER_FUEL
+          .getIcon(packet.getNightMode() != null && packet.getNightMode()));
+      this.nightMode = packet.getNightMode();
+    }
 
+    // update speed
     if (packet.getSpeed() != null) {
-      drawSpeed(packet);
+      this.content.getSpeed().setText(String.valueOf(packet.getSpeed()));
+      this.speed = packet.getSpeed();
     }
 
+    // update fuel
     if (packet.getFuelPercent() != null) {
-      drawFuel(packet);
+      this.fuel = packet.getFuelPercent();
     }
 
+    // update info text
     drawInfo(packet);
 
+    // update speed limiter
     if (packet.getLimiterSpeed() != null) {
       drawSpeedLimiter(packet);
     }
 
+    // update engine state
     if (packet.getEngineState() != null) {
       drawWarning(packet);
     }
-  }
-
-  private void drawSpeedometer(final VehicleDisplayPacket packet)  {
-    this.content.getBackground().icon().set(VehicleDisplayTexture.SPEEDOMETER_BACKGROUND
-        .getIcon(packet.getNightMode() != null && packet.getNightMode()));
-  }
-
-  private void drawSpeed(final VehicleDisplayPacket packet) {
-    this.content.getSpeed().setText(String.valueOf(packet.getSpeed()));
-
-    // ToDo: Draw line
-  }
-
-  private void drawFuel(final VehicleDisplayPacket packet) {
-    this.content.getFuel().icon().set(VehicleDisplayTexture.FUEL_OVERLAY
-        .getIcon(packet.getNightMode() != null && packet.getNightMode()));
-
-    // ToDo: Draw line
   }
 
   private void drawInfo(final VehicleDisplayPacket packet) {
@@ -126,7 +175,11 @@ public class VehicleDisplayWidget extends WidgetHudWidget<HudWidgetConfig> imple
       this.content.getSpeedLimiter().icon().set(icon);
       this.content.getSpeedLimiter().setVisible(true);
 
-      // ToDo: Draw line
+      if (packet.getLimiterSpeed() != null) {
+        this.limiterActive = packet.getLimiterActive();
+      }
+
+      this.limiterSpeed = packet.getLimiterSpeed();
     }
   }
 
@@ -139,9 +192,8 @@ public class VehicleDisplayWidget extends WidgetHudWidget<HudWidgetConfig> imple
               .getIcon(packet.getNightMode() != null && packet.getNightMode());
 
       this.content.getWarning().icon().set(icon);
-      this.content.getWarning().setVisible(true);
-
-      // ToDo: Blinking
+      this.damageState = packet.getDamageState();
+      this.warningLight = 0;
     }
   }
 
