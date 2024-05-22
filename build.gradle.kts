aad11861caa8dffdac8ab8fe2b59a480d59c0a53
Funboyy <72627658+Funboyy@users.noreply.github.com>
2024-05-22 12:47:33 +0200

    updated to Java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,6 @@ plugins {
 group = "de.germanminer"
 version = "1.0.0"
 
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
-
 labyMod {
     defaultPackageName = "de.germanminer.addon" //change this to your main package name (used by all modules)
     addonInfo {
@@ -63,7 +61,7 @@ fun configureRun(provider: net.labymod.gradle.core.minecraft.provider.VersionPro
 
     provider.javaVersion = when (gameVersion) {
         else -> {
-            JavaVersion.VERSION_17
+            JavaVersion.VERSION_21
         }
     }
 
